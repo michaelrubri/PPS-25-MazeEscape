f4bee8e1a5e69322cb3a04569ddf1bf634488e98
Michael Rubri <michael.rubri@studio.unibo.it>
2025-07-12 10:54:29 +0200

    edited controller and view

--- src/main/scala/view/GameView.java
+++ src/main/scala/view/GameView.java
@@ -1,8 +1,7 @@
 package view;
 
 import model.Game;
-import model.map.*; // importa tus celdas y Maze aquí
-
+import model.map.*;
 import javax.swing.*;
 import java.awt.*;
 import java.util.HashMap;
