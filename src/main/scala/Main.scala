6bd877c39224d1d180550fd422c90c0e1bb4bd0b
Michael Rubri <michael.rubri@studio.unibo.it>
2025-07-21 17:00:11 +0200

    edited main

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -10,11 +10,9 @@ import controller.*
 import java.util.function.Consumer
 
 object Main extends App:
-     new MenuView(new Consumer[String] {
-       override def accept(difficulty: String): Unit = {
-         val gameSettings: GameSettings = GameSettings.fromDifficulty(difficulty)
-         val game: Game = Game(gameSettings)
-         val view: GameView = GameView(game)
-         val controller: Controller = Controller(view, game)
-       }
+     new MenuView((difficulty: String) => {
+       val gameSettings: GameSettings = GameSettings.fromDifficulty(difficulty)
+       val game: Game = Game(gameSettings)
+       val view: GameView = GameView(game)
+       val controller: Controller = Controller(view, game)
      })
