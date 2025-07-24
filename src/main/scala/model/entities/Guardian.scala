3cb913cc44bb563ed4a58ad05d12c6b467d763a1
Michael Rubri <michael.rubri@studio.unibo.it>
2025-07-24 23:41:44 +0200

    edited guardian and guardian test

--- src/main/scala/model/entities/Guardian.scala
+++ src/main/scala/model/entities/Guardian.scala
@@ -3,7 +3,10 @@
  * Licensed under the MIT License
  */
 
-package model
+package model.entities
+
+import model.utils.Position
+import model.utils.Position._
 
 /**
  * Represents the NPC character ruled by the system.
@@ -11,34 +14,39 @@ package model
 trait Guardian extends Entity:
 
   /**
-   * The current position of the guardian.
-   */
-  def position: (Int, Int)
-
-  /**
    * Defines the new position of the guardian, trying to reach the player.
    *
    * @param target the position of the player.
+   * @return new set of coordinates.
    */
-  def intercept(target: (Int, Int)): (Int, Int)
+  def intercept(target: Position): Position
 
   /**
    * Used by the Game module to update the position of the guardian.
    *
    * @param newPosition the coordinates of the new position.
+   * @return updated instance of the guardian.
    */
-  private[model] def updatePosition(newPosition: (Int, Int)): Unit
+  private[model] def updatePosition(newPosition: Position): Guardian
 
+/**
+ * The companion object of guardian.
+ */
 object Guardian:
-  def apply(initialPosition: (Int, Int)): Guardian =
-    GuardianImpl(initialPosition)
 
-private case class GuardianImpl(private var _position: (Int, Int)) extends Guardian:
-  override def position: (Int, Int) = _position
-  override def intercept(target: (Int, Int)): (Int, Int) =
-    val (dx, dy) = (target._1 - _position._1, target._2 - _position._2)
+  /**
+   * Generates a new instance of guardian.
+   *
+   * @param initialPosition guardian's starting position.
+   * @return new instance of guardian.
+   */
+  def apply(initialPosition: Position): Guardian = GuardianImpl(initialPosition)
+
+private case class GuardianImpl(position: Position) extends Guardian:
+  override def intercept(target: Position): Position =
+    val (dx, dy) = (target.x - position.x, target.y - position.y)
     (dx, dy) match
-      case (dx, dy) if dx.abs + dy.abs <= 1 => (_position._1, _position._2)
-      case (dx, dy) if dx.abs >= dy.abs => (_position._1 + dx.sign, _position._2)
-      case (_, dy) => (_position._1, _position._2 + dy.sign)
-  override private[model] def updatePosition(newPosition: (Int, Int)): Unit = _position = newPosition
+      case (dx, dy) if dx.abs + dy.abs <= 1 => Position(position.x, position.y)
+      case (dx, dy) if dx.abs >= dy.abs => Position(position.x + dx.sign, position.y)
+      case (_, dy) => Position(position.x, position.y + dy.sign)
+  override private[model] def updatePosition(newPosition: Position): Guardian = copy(position = newPosition)
--- src/test/scala/model/entities/GuardianTest.scala
+++ src/test/scala/model/entities/GuardianTest.scala
@@ -3,12 +3,11 @@
  * Licensed under the MIT License
  */
 
-package model
+package model.entities
 
-import model.entities.Guardian
+import model.utils.Position
 import org.junit.jupiter.api.*
 import org.junit.jupiter.api.Assertions.*
-
 import scala.compiletime.uninitialized
 
 class GuardianTest:
@@ -16,34 +15,34 @@ class GuardianTest:
   var guardian: Guardian = uninitialized
 
   @BeforeEach
-  def init(): Unit = guardian = Guardian((0, 0))
+  def init(): Unit = guardian = Guardian(Position(0, 0))
 
   @Test
-  def testInitialValues(): Unit = assertEquals((0, 0), guardian.position)
+  def testInitialValues(): Unit = assertEquals(Position(0, 0), guardian.position)
 
   @Test
   def testGuardianAdjacentPlayerNotMove(): Unit =
-    val adjacentHorizontally = guardian.intercept(1, 0)
-    assertEquals((0, 0), adjacentHorizontally,"Guardian should not move")
-    val adjacentVertically = guardian.intercept(0, 1)
+    val adjacentHorizontally = guardian.intercept(Position(1, 0))
+    assertEquals(Position(0, 0), adjacentHorizontally,"Guardian should not move")
+    val adjacentVertically = guardian.intercept(Position(0, 1))
     assertEquals((0, 0), adjacentVertically, "Guardian should not move")
 
   @Test
   def testInterceptHorizontally(): Unit =
-    val moveRight = guardian.intercept(3, 0)
-    assertEquals((1, 0), moveRight, "Guardian should move to the right")
-    val moveLeft = guardian.intercept(-3, 0)
-    assertEquals((-1, 0), moveLeft, "Guardian should move to the left")
+    val moveRight = guardian.intercept(Position(3, 0))
+    assertEquals(Position(1, 0), moveRight, "Guardian should move to the right")
+    val moveLeft = guardian.intercept(Position(-3, 0))
+    assertEquals(Position(-1, 0), moveLeft, "Guardian should move to the left")
 
   @Test
   def testInterceptVertically(): Unit =
-    val moveUp = guardian.intercept(0, 3)
-    assertEquals((0, 1), moveUp, "Guardian should move to the top")
-    val moveDown = guardian.intercept(0, -3)
-    assertEquals((0, -1), moveDown, "Guardian should move to the bottom")
+    val moveUp = guardian.intercept(Position(0, 3))
+    assertEquals(Position(0, 1), moveUp, "Guardian should move to the top")
+    val moveDown = guardian.intercept(Position(0, -3))
+    assertEquals(Position(0, -1), moveDown, "Guardian should move to the bottom")
 
   @Test
   def testUpdatePosition(): Unit =
-    val position = (1,1)
+    val position = Position(2, 2)
     guardian.updatePosition(position)
     assertEquals(position, guardian.position, "The guardian should be in the new position")
