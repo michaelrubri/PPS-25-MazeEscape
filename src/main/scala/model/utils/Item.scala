569c8c6e15a0323224e0de651044757f90bf5986
Michael Rubri <michael.rubri@studio.unibo.it>
2025-08-01 17:21:44 +0200

    added items

---
+++ src/main/scala/model/utils/Item.scala
@@ -0,0 +1,31 @@
+package model.utils
+
+import model.entities.Player
+
+sealed trait Item:
+  def id: String
+  def name: String
+  def description: String
+
+trait Stackable[T]:
+  def maxStack(item: T): Int
+
+trait Usable[T]:
+  def use(item: T, player: Player): Player
+
+case object SealOfTheChallenge extends Item:
+  val id = "special-key"
+  val name = "Seal of the challenge"
+  val description = "Unlock access to the Mysterious Man"
+
+case class InvisibilityPotion(turns: Int = 3) extends Item:
+  val id = "inv-potion"
+  val name = "Invisibility potion"
+  val description = s"Makes invisible for $turns turns"
+  
+given Stackable[InvisibilityPotion] with
+  override def maxStack(item: InvisibilityPotion): Int = 5
+  
+given Usable[InvisibilityPotion] with
+  override def use(item: InvisibilityPotion, player: Player): Player =
+    player.addStatus("invisible", item.turns)
